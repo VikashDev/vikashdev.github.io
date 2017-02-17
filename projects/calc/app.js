39d50b102c8e1f7988dec40cbd43af9e3a49e6f4
Vikash Kumar <vikash087@gmail.com>
2017-02-18 03:21:33 +0530

    added simple calculator app

--- projects/calc/app.js
+++ projects/calc/app.js
@@ -1,2 +1,15 @@
 console.log('app.js loaded in ');
 let buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+
+let resultBox = document.getElementById('result');
+
+
+ buttons[0].addEventListener('click', (e) => {
+        e.preventDefault() ;
+         resultBox.value +=  buttons[0].value;
+     });
+
+buttons[1].addEventListener('click', () => {
+        resultBox.value = buttons[1].value;
+});
