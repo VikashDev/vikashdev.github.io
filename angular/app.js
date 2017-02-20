784cb04fbae7cc3e48a8a71be030c78325ac05d4
Vikash Kumar <VikashDev@users.noreply.github.com>
2017-02-21 01:00:44 +0530

    testing

--- angular/app.js
+++ angular/app.js
@@ -3,7 +3,7 @@ let app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
     $scope.data2 = 'demo data 2 here';
 
-    $http.get('https://c-search-api.herokuapp.com/search/c/C++').then((res) => {
+    $http.jsonp('https://c-search-api.herokuapp.com/search/c/C++').then((res) => {
         $scope.data = res.data;
     }, (err) => {
         console.log(err);
